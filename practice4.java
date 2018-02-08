6e0a69073658d460772e641198faa8c60df17f02
ElijahColwill <elijah.c035@gmail.com>
2018-02-07 22:05:39 -0700

    Finish

--- practice4.java
+++ practice4.java
@@ -72,28 +72,66 @@ class ColSums
                      {9, 1, 0, 2},
                      {0, 2, 6, 3, -1, -8} };
     
-    int rows = 0;
+    int max = data[0].length;
 
     for (int i=0; i < data.length; i++) {
-    	rows++;
+      if (data[i].length > max) {max = data[i].length;}
     }
 
-    int[] ColSums = new int[rows];  
+    int[] ColSums = new int[max];
 
-    for ( int row=0; row < ColSums.length; row++)
+    for ( int row=0; row < data.length; row++)
     {
-      // initialize the sum
-      ColSums[row] = 0;
-      
-      // compute the sum for this row
-      for ( int col=0; col < data.length; col++) 
+      for ( int col=0; col < data[row].length; col++) 
       {
-         ColSums[row] = ColSums[row] + data[col][row];
+        ColSums[col] += data[row][col];
       }
-      
-      // write the sum for this row
-      System.out.println("Sum for this Column: " + ColSums[row]);
     }
-      
+
+    for (int i=0; i < ColSums.length; i++) {
+      System.out.println("Sum for the column: " + ColSums[i]);
+    }
+
   }
-}    
+      
+}
+
+class ReverseRow
+{
+  static int[][] data = { {3, 2, 5},
+                     {1, 4, 4, 8, 13},
+                     {9, 1, 0, 2},
+                     {0, 2, 6, 3, -1, 8},
+                     {-1, -2, -3, 4, 5, 45  }, 
+                     {56},
+                     {0, 1, 2, 3, 4, 5, 6, 7} };                    
+
+  private static void printArray()
+  {
+    for ( int row=0; row < data.length; row++)
+    {
+      for ( int col=0; col < data[row].length-1; col++) 
+         System.out.print( data[row][col] + ", " );
+      System.out.println( data[row][data[row].length-1] ); 
+    }
+  }
+  
+  public static void main ( String[] args ) 
+  { 
+    // print out the initial array
+    printArray();
+    System.out.println();
+
+    for(int row = 0; row < data.length; row++){
+      for(int col = 0; col < data[row].length / 2; col++) {
+        int temp = data[row][col];
+        data[row][col] = data[row][data[row].length - col - 1];
+        data[row][data[row].length - col - 1] = temp;
+        }
+    }
+
+    // print out the reversed array
+    printArray();
+  }
+}  
+
